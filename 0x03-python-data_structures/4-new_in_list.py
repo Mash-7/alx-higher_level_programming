6d647e7c1f5d47a3d145ba62aaf99126f516f236
Mash-7 <mashoratile@gmail.com>
2022-08-29 19:28:02 -0700

    check 6 and 11

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    replacement = my_list[:]
+    if idx <= 0 or idx > (len(my_list) - 1):
+        return (my_list)
 
-    if idx <= 0 or idx < (len(my_list) - 1):
-        replacement[idx] = element
-
+    replacement = [x for x in my_list]
+    replacement[idx] = element
     return (replacement)
