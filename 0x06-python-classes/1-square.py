073caa660f8ae8071a661a9d5d24b382449cb8d7
Mash-7 <mashoratile@gmail.com>
2022-09-13 16:05:33 -0700

    task 1 fix

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a new square.
+
+        Args:
+            size (int): Size of the new square.
+        """
+        self.__size = size
