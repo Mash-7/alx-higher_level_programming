2da90913ec8296ec353a29f745a7e96547655c5f
Mash-7 <mashoratile@gmail.com>
2022-09-12 18:52:23 -0700

    bytecode check 1

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,7 +3,7 @@ def magic_calculation(a, b):
     owari = 0
     for n in range(1, 3):
         try:
-            if (n > a):
+            if n > a:
                 raise Exception("Too far")
             else:
                 owari += (a ** b) / n
