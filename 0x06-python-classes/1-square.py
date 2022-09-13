3b1b7ffcbd767a57e4cda12b94ede03c98939682
Mash-7 <mashoratile@gmail.com>
2022-09-13 15:20:17 -0700

    task 1 fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/pyhon3
 # 1-square.py
-"""Class square that defines a square"""
+"""Defines a square"""
+
+
 class Square:
     """This represents a square."""
+
     def __init__(self, size):
         """Initializes a new square.
 
