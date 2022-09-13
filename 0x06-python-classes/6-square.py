1194c47df2ebd364396cb81675a266622d40b1ac
Mash-7 <mashoratile@gmail.com>
2022-09-13 16:51:52 -0700

    fix my_print section

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -55,14 +55,12 @@ class Square:
 
     def my_print(self):
         """Prints a # square according to the size value"""
-        if self.size == 0:
-            print()
-        else:
-            for  i in range(self.position[1]):
-                print()
-            for i in range(0, self.size):
-                for k in range(self.position[0]):
-                    print(" ", end='')
-                    for j in range(self.size):
-                        print("#", end='')
-                        print()
+        if self.__size == 0:
+            print("")
+            return
+
+        [print ("") for i in range(self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
